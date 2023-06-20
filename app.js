eba120b61d3be02abe3079c4b676c6e962a1fb7f
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-20 19:03:06 +0200

    displayQuestion

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const displayQuestion = () => {
     showScores();
   } else {
     // show question
+    let questionElement = document.getElementById("question");
+    questionElement.innerHTML = Quiz.getQuestionIndex().text;
+
+    // show options
+    let choices = Quiz.getQuestionIndex().choices;
   }
 };
