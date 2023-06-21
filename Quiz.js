f986a502a2d28ecbe4054f2486aeb40a88ec9ea8
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-21 17:48:46 +0200

    questions

--- Quiz.js
+++ Quiz.js
@@ -20,34 +20,3 @@ class Quiz {
     return this.getQuestionIndex === this.questions.length;
   }
 }
-
-// CREATE QIUZ QUESTIONS
-let questions = [
-  new Question(
-    "Hyper Text Markup Language Stands For?",
-    ["JQuery", "XHTML", "CSS", "HTML"],
-    "HTML"
-  ),
-  new Question(
-    "Cascading Style sheet Stands For?",
-    ["HTML", "JQuery", "CSS", "XML"],
-    "CSS"
-  ),
-  new Question(
-    "Which is a JavaScript Framework?",
-    ["React", "Laravel", "Django", "Sass"],
-    "React"
-  ),
-  new Question(
-    "Which is a backend Language?",
-    ["PHP", "HTML", "React", "All"],
-    "PHP"
-  ),
-  new Question(
-    "Which is best for Artificial intelligence?",
-    ["React", "Laravel", "Python", "Sass"],
-    "Python"
-  ),
-];
-
-quiz(questions);
--- app.js
+++ app.js
@@ -48,3 +48,34 @@ const showScores = () => {
   let quizElement = document.getElementById("quiz");
   quizElement.innerHTML = quizEndHTML;
 };
+
+// CREATE QIUZ QUESTIONS
+let questions = [
+  new Question(
+    "Hyper Text Markup Language Stands For?",
+    ["JQuery", "XHTML", "CSS", "HTML"],
+    "HTML"
+  ),
+  new Question(
+    "Cascading Style sheet Stands For?",
+    ["HTML", "JQuery", "CSS", "XML"],
+    "CSS"
+  ),
+  new Question(
+    "Which is a JavaScript Framework?",
+    ["React", "Laravel", "Django", "Sass"],
+    "React"
+  ),
+  new Question(
+    "Which is a backend Language?",
+    ["PHP", "HTML", "React", "All"],
+    "PHP"
+  ),
+  new Question(
+    "Which is best for Artificial intelligence?",
+    ["React", "Laravel", "Python", "Sass"],
+    "Python"
+  ),
+];
+
+quiz(questions);
