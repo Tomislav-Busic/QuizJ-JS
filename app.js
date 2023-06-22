a22be2d8594db99e4c01dd263e6d888ebe13a12d
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-22 17:20:50 +0200

    animations

--- app.js
+++ app.js
@@ -105,23 +105,3 @@ const startCountDown = () => {
 };
 
 startCountDown();
-
-const animatonChoices = () => {
-  let buttons = document.getElementById("buttons");
-  let button1 = document.getElementById("btn0");
-  let button2 = document.getElementById("btn1");
-  let button3 = document.getElementById("btn2");
-  let button4 = document.getElementById("btn3");
-
-  button1.classList.add("box1");
-  button2.classList.add("box2");
-  button3.classList.add("box3");
-  button4.classList.add("box4");
-
-  buttons.addEventListener("animationend", () => {
-    button1.classList.remove("box1");
-    button2.classList.remove("box2");
-    button3.classList.remove("box3");
-    button4.classList.remove("box4");
-  });
-};
