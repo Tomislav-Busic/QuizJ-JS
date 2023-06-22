042076214d42b46d1d20afee009edf67d13e1ca9
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-22 16:54:30 +0200

    slide in choices

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const displayQuestion = () => {
 const guess = (id, guess) => {
   let button = document.getElementById(id);
   button.onclick = () => {
+    animatonChoices();
     quiz.guess(guess);
     displayQuestion();
   };
@@ -104,3 +105,23 @@ const startCountDown = () => {
 };
 
 startCountDown();
+
+const animatonChoices = () => {
+  let buttons = document.getElementById("buttons");
+  let button1 = document.getElementById("btn0");
+  let button2 = document.getElementById("btn1");
+  let button3 = document.getElementById("btn2");
+  let button4 = document.getElementById("btn3");
+
+  button1.classList.add("box1");
+  button2.classList.add("box2");
+  button3.classList.add("box1");
+  button4.classList.add("box2");
+
+  buttons.addEventListener("animationend", () => {
+    button1.classList.remove("box1");
+    button2.classList.remove("box2");
+    button3.classList.remove("box1");
+    button4.classList.remove("box2");
+  });
+};
